2901646471a97a70106194fe5d548ea191090746
duca <holaduca@gmail.com>
2022-08-21 13:30:42 -0300

    hola

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const botonSuspenso = addEventListener("click", function() {
-    document.getElementById("resultadoBoton").innerHTML = "Promising young woman: Cassie vive una doble vida hasta que un encuentro inesperado le da la oportunidad de corregir errores del pasado";
+
+
+let botonTerror = addEventListener("click", function() {
+    Swal.fire({
+        title: 'Get out',
+        text: 'Un joven va por primera vez a la casa de sus futuros suegros, una serie de descubrimientos hacen ese encuentro inquietante',
+        imageUrl: 'https://pics.filmaffinity.com/D_jame_salir-290175782-mmed.jpg',
+        imageWidth: 200,
+        imageHeight: 300,
+        imageAlt: 'Custom image',
+      })
+});
+
 
