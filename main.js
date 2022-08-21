a19f0058c2ae97814b077e3ea1d4cabd4bb94ac1
duca <holaduca@gmail.com>
2022-08-21 01:32:59 -0300

    librerias

---
+++ main.js
@@ -0,0 +1,3 @@
+const botonSuspenso = addEventListener("click", function() {
+    document.getElementById("resultadoBoton").innerHTML = "Promising young woman: Cassie vive una doble vida hasta que un encuentro inesperado le da la oportunidad de corregir errores del pasado";
+
